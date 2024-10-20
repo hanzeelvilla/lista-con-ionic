3d8a359beedc659ade4ec47b14a0b138eea824d0
Hanzeel Villa <hanzeelvilla0@gmail.com>
2024-10-20 09:03:59 -0600

    agregar usuarios

--- src/app/usuarios/usuarios.page.ts
+++ src/app/usuarios/usuarios.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, IonModal } from '@ionic/angular';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,13 +9,84 @@ import { Router } from '@angular/router';
 })
 export class UsuariosPage implements OnInit {
 
-  constructor(private router: Router) { }
+  nombre: string = "";
+  direccion: string = "";
+  correo: string = "";
+  telefono: string = "";
+  imagen: string = "";
+  usuarios: Array<{ nombre: string; direccion: string; correo: string; telefono: string; imagen: string }> = [];
+
+  constructor(private alertController: AlertController, private router: Router) {
+    this.cargarProdcutosDeLocalStorage();
+   }
 
   ngOnInit() {
   }
 
+  /* -------------------------- LOGICA CRUD USUARIOS -------------------------- */
+
+  async guardar() {
+    if(!this.nombre || !this.direccion || !this.correo || !this.telefono || !this.imagen) {
+      await this.alerta("Error", "No puedes dejar campos vacios");
+      return; // salir de la funcion si hay campos vacios
+    }
+
+    const usuario = {
+      nombre: this.nombre,
+      direccion: this.direccion,
+      correo: this.correo,
+      telefono: this.telefono,
+      imagen: this.imagen
+    };
+
+    this.usuarios.push(usuario);
+    this.guardarEnLocalStorage();
+    this.limpiarCampos();
+    this.alerta("Agregado", "usuario agregado");
+  }
+
+  editar() {
+    console.log("editar");
+  }
+
+  eliminar() {
+    console.log("eliminar");
+  }
+
+  async alerta(header: string, message: string) {
+    const alert = await this.alertController.create ({
+      header: header,
+      message: message,
+      buttons: ["OK"]  
+    });
+
+    await alert.present();
+  }
+
+  limpiarCampos() {
+    this.nombre = "",
+    this.direccion = "",
+    this.correo = "",
+    this.telefono = ""
+    this.imagen = ""
+  }
+
+  /* --------------------------- LOGICA LOCALSTORAGE -------------------------- */
+
+  guardarEnLocalStorage() {
+    localStorage.setItem("usuarios", JSON.stringify(this.usuarios));
+  }
+
+  cargarProdcutosDeLocalStorage() {
+    const usuariosLocalStorage = localStorage.getItem("usuarios");
+    if(usuariosLocalStorage)
+      this.usuarios = JSON.parse(usuariosLocalStorage);
+  }
+
+  /* ---------------------------- LOGICA NAVEGACION --------------------------- */
+
   paginaProductos() {
-    this.router.navigate(["home"])
+    this.router.navigate(["home"]);
   }
 
 }
